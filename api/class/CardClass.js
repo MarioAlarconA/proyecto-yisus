8d3fc8f6d4f2679045183eed6d6a21ab92287bb0
MarioAlarconA <utm23090584@utma.edu.mx>
2024-07-25 13:51:03 -0600

    25/07 creacion de clase cuentas y tarjetas

---
+++ api/class/CardClass.js
@@ -0,0 +1,54 @@
+//Crear tarjeta, obtener
+import CardModel from "../models/CardModel.js"
+class ManagerCard{
+    constructor (
+        userId,
+        accountId,
+        cardNumber,
+        cardType,
+        expirationDate,
+        securityCode,
+        status) {
+            this.userId = userId;
+            this.accountId = accountId;
+            this.cardNumber = cardNumber;
+            this.cardType = cardType;
+            this.expirationDate = expirationDate;
+            this.securityCode = securityCode;
+            this.status = status;
+    }
+
+    async createCard(){
+        try {
+            await CardModel.create({
+                userId:this.userId,
+                accountId:this.accountId,
+                cardNumber:this.cardNumber,
+                cardType:this.cardType,
+                expirationDate:this.expirationDate,
+                securityCode:this.securityCode,
+                status:this.status
+            });
+            return "OK";
+        } catch (error) {
+            throw new Error(`Error al crear tarjeta: ${error}`)
+        }
+    }
+    async getCards(){
+        try {
+            const cards = await CardModel.find();
+            return cards;
+        } catch (error) {
+            throw new Error(`Error al obtener cuentas: ${error}`)
+        }
+    }
+
+    async getCard(id){
+        try {
+            const card = await CardModel.findById(id);
+            return card;
+        } catch (error) {
+            throw new Error(`Error al obtener cuentas: ${error}`)
+        }
+    }
+}
